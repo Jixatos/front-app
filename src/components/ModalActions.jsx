e1419513bf5307e6083063dea23653ef4ef6d5b9
jixatos <j.gody2004@gmail.com>
2023-09-30 18:15:41 -0300

    Estilização do modal + reset no App.

--- src/components/ModalActions.jsx
+++ src/components/ModalActions.jsx
@@ -1,11 +1,13 @@
-export default function ModalActions(props){
+import styles from "../css/ModalActions.module.css";
 
-    if(props.open) {
-        return(
-            <>
-                <h1>MODAL PRODUTOS</h1>
-                <button onClick={()=> props.setOpen(false)}>CLOSE-MODAL</button>
-            </>
-        )
-    }
+export default function ModalActions(props) {
+    
+  if (props.open) {
+    return (
+      <div className={styles.container}>
+        <h1>MODAL PRODUTOS</h1>
+        <button onClick={() => props.setOpen(false)}>CLOSE-MODAL</button>
+      </div>
+    );
+  }
 }
